cec0e655e846c35a21f3a5765d84633d4e96d3f4
Loup <loupmayeur2003@gmail.com>
2023-01-10 17:39:19 +0100

    essaie donwload qr

--- lib/qrCodePage.dart
+++ lib/qrCodePage.dart
@@ -1,8 +1,9 @@
+import 'dart:io';
 import 'dart:ui';
-
+import 'package:download_assets/download_assets.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:stage/local.dart';
+import 'package:http/http.dart' as http;
 
 class QrCodePage extends StatefulWidget {
   const QrCodePage({super.key, required this.title});
@@ -14,7 +15,22 @@ class QrCodePage extends StatefulWidget {
 }
 
 class QrCodePageState extends State<QrCodePage> {
+  DownloadAssetsController downloadAssetsController =
+      DownloadAssetsController();
+  String message = "Press the download button to start the download";
+  bool downloaded = false;
   String _dataStr = 'Erreur';
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future _init() async {
+    await downloadAssetsController.init();
+    downloaded = await downloadAssetsController.assetsDirAlreadyExists();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +58,28 @@ class QrCodePageState extends State<QrCodePage> {
           ],
         ),
       ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: _refresh,
+            tooltip: 'Télécharger',
+            child: const Icon(Icons.download),
+          ),
+        ],
+      ),
     );
+  }
+
+  Future _refresh() async {
+    await _downloadFile(
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKw5RDspUDcObjGF6t2fY8WKSJnNhVfzFRoMHL3GsT&s',
+        'QrCode');
+  }
+
+  Future<File> _downloadFile(String url, String fileName) async {
+    var req = await http.Client().get(Uri.parse(url));
+    var file = File('C:/Users/pc-Utilisateur/Documents/$fileName.png');
+    return file.writeAsBytes(req.bodyBytes, flush: true);
   }
 }
